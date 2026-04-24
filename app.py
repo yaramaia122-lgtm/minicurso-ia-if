ff6b2184f546d361bdbdc78004c728b7b31624b0
yaramaia122-lgtm <yaramaia122@gmail.com>
2026-04-24 18:01:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 import streamlit as st
-st.set_page_config(page_title="Portal do Aluno", page_icon="🎓")
-st.sidebar.title("👨‍💻 Desenvolvedor")
+
+st.set_page_config(page_title="Portal do Aluno", page_icon=" ")
+st.sidebar.title(" Desenvolvedor")
 st.sidebar.write("Minicurso IA para Devs")
-st.title("🎓 Simulador de Desempenho")
+
+st.title(" Simulador de Desempenho")
+
 c1, c2 = st.columns(2)
 with c1: n1 = st.text_input("Nota 1", value="0")
-with c2: n2 = st.text_input("Nota 2", value="0")
-if st.button("Analisar Notas", use_container_width=True):
-try:
-resultado = (float(n1) + float(n2)) / 2
-if resultado >= 6.0:
-st.success(f"Média: {resultado:.1f} - APROVADO! 🎉")
-else:
-st.error(f"Média: {resultado:.1f} - RECUPERAÇÃO 📚")
-except ValueError:
-st.warning("⚠️ Erro: Introduza apenas números (use ponto para decimais).")
+  with c2: n2 = st.text_input("Nota 2", value="0")
+    if st.button("Analisar Notas", use_container_width=True):
+      try:
+        resultado = (float(n1) + float(n2)) / 2
+        if resultado >= 6.0:
+          st.success(f"Média: {resultado:.1f} - APROVADO! ")
+        else:
+          st.error(f"Média: {resultado:.1f} - RECUPERAÇÃO ")
+      except ValueError:
+        st.warning(" Erro: Introduza apenas números (use ponto para decimais).")
