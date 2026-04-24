f096753c4f418ef0a285f040913296cf07520e98
yaramaia122-lgtm <yaramaia122@gmail.com>
2026-04-24 14:22:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,10 @@
 import streamlit as st
 
-st.set_page_config(page_title="Portal do Aluno", page_icon=" ")
-st.sidebar.title(" Desenvolvedor")
-st.sidebar.write("Minicurso IA para Devs")
+st.title("Calculadora de Notas do IF")
 
-st.title(" Simulador de Desempenho")
+n1 = st.text_input("Introduza a Nota 1")
+n2 = st.text_input("Introduza a Nota 2")
 
-c1, c2 = st.columns(2)
-with c1: n1 = st.text_input("Nota 1", value="0")
-with c2: n2 = st.text_input("Nota 2", value="0")
-with c3: n3 = st.text_input("Nota 3", value="0")
-  
-if st.button("Analisar Notas", use_container_width=True):
-  try:
-    resultado = (float(n1) + float(n2)) / 2
-    if resultado >= 6.0:
-      st.success(f"Média: {resultado:.1f} - APROVADO! ")
-    else:
-      st.error(f"Média: {resultado:.1f} - RECUPERAÇÃO ")
-  except ValueError:
-    st.warning(" Erro: Introduza apenas números (use ponto para decimais).")
+if st.button("Calcular Média"):
+  media = (float(n1) + float(n2)) / 2
+  st.write("A tua média é:", media)
